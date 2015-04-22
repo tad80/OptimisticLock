ec6fc9b193adbb2c63514e09fda49f9676c519e3
Nakamura, Tadashi a | Tad | GCSD <tadashi.a.nakamura@rakuten.com>
2015-04-22 22:11:43 +0900

    bugfix

--- Model/Behavior/OptimisticLockBehavior.php
+++ Model/Behavior/OptimisticLockBehavior.php
@@ -38,8 +38,8 @@ class OptimisticLockBehavior extends ModelBehavior {
 	}
 
 	public function beforeSave(Model $model, $options = array()) {
-		if (isset($model->data[$this->alias]['opt_' . $this->config['field']])) {
-			unset($model->data[$this->alias]['opt_' . $this->config['field']]);
+		if (isset($model->data[$model->alias]['opt_' . $this->config['field']])) {
+			unset($model->data[$model->alias]['opt_' . $this->config['field']]);
 		}
 		return true;
 	}
